1e2d7ca864b6e2c87fda1cdae9c326fe1d0197ef
LCSmith87 <LCSmith87@gmail.com>
2020-05-14 13:23:49 -0500

    Initial commit - added html5 boilerplate styles and layout component

---
+++ components/Layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+import styled from 'styled-components'
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default Layout
